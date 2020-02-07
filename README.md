24ee914150363736fc7e90c98c903d8f25cad0fe
HermSidhu <h-sidhu@hotmail.com>
2020-02-07 00:10:05 -0500

    reorganized

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # To-Do
+
+To-Do is a full stack web application that helps you stay organized, built with JavaScript, HTML, & CSS.
+
--- weather.js
+++ weather.js
@@ -1,4 +1,5 @@
 // Weather in Header
+require('dotenv').config()
 
 // SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
@@ -17,7 +18,7 @@ weather.temperature = {
 // APP CONSTS AND VARS
 const KELVIN = 273;
 // API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
+const key = process.env.Weahter_API_Key
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if('geolocation' in navigator){
